f387787f920a683609a960bf66707a334b994fde
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2024-07-14 12:12:56 +0530

    made hero section for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,39 @@ export default function Home() {
   return (
     <>
       <header>
-        <div className="w-screen h-10 flex items-center justify-around">
+        <div className="w-screen my-4 flex items-center justify-around">
           <div>FlashQuizzr</div>
           <nav>
-            <ul className="flex justify-around gap-x-4">
+            <ul className="flex justify-around items-center gap-x-4">
               <li>Feature</li>
               <li>Pricing</li>
               <li>Sigin</li>
-              <li>Signup</li>
+              <li><a><button className="py-2 px-6 text-white bg-cyan-600 rounded-full">Signup</button></a></li>
             </ul>
           </nav>
         </div>
       </header>
-      <main className="flex min-h-screen flex-col items-center justify-start px-24">
+      <main className="flex min-h-screen w-screen flex-col items-center justify-start">
 
-        <section id="hero"></section>
-        <section id="feature"></section>
-        <section id="pricing"></section>
+        <section id="hero" className="w-screen h-[80vh] bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)]">
+          <div className="h-full flex flex-col items-center justify-center">
+            <h1 className="text-5xl font-bold">Ace Your Exams</h1>
+            <p className="text-lg mt-8">Generate Flashcards and Quizzes with AI to prepare for your exams.</p>
+            <div className="mt-20 flex gap-x-8">
+              <a>
+                <button className="py-2 px-8 rounded-full bg-white">Learn More</button>
+              </a>
+              <a>
+                <button className="py-2 px-8 rounded-full text-white bg-cyan-600">Generate Flashcards</button>
+              </a>
+            </div>
+          </div>
+        </section>
+        <section id="feature" className="w-screen h-[80vh] bg-[rgb(189,240,255)] bg-[radial-gradient(circle, rgba(189,240,255,1) 0%, rgba(234,255,213,1) 100%)]"></section>
+        <section id="testemonial" className="w-1/2 h-8 bg-green-400"></section>
+        <section id="pricing" className="w-1/2 h-20 bg-blue-300"></section>
+        <section id="cta" className="w-1/2 h-10 bg-yellow-300"></section>
+
       </main>
       <footer>
         <div>
