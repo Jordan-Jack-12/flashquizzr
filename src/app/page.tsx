3478857b8ccb2cb449ca280653319e1a694e67e5
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2024-07-17 18:50:43 +0530

    added footer changed text sizes added fixed position to topbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <header>
-        <div className="w-screen my-4 flex items-center justify-around">
+        <div className="fixed top-0 right-0 left-0 z-50 bg-white w-screen py-4 flex items-center justify-around font-bold">
           <div>FlashQuizzr</div>
           <nav>
             <ul className="flex justify-around items-center gap-x-4">
@@ -24,28 +24,28 @@ export default function Home() {
 
         <section id="hero" className="w-screen h-[80vh] bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)]">
           <div className="h-full flex flex-col items-center justify-center">
-            <h1 className="text-5xl font-bold">Ace Your Exams</h1>
-            <p className="text-lg mt-8">Generate Flashcards and Quizzes with AI to prepare for your exams.</p>
+            <h1 className="text-5xl font-bold tracking-tighter">Ace Your Exams</h1>
+            <p className="text-lg mt-8 text-slate-500">Generate Flashcards and Quizzes with AI to prepare for your exams.</p>
             <div className="mt-20 flex gap-x-8">
-              <a>
-                <button className="py-2 px-8 rounded-full bg-white">Learn More</button>
-              </a>
-              <a>
-                <button className="py-2 px-8 rounded-full text-white bg-cyan-500">Generate Flashcards</button>
-              </a>
+              <Link href={""}>
+                <button className="py-2 px-8 rounded-full bg-white">Watch Demo</button>
+              </Link>
+              <Link href={""}>
+                <button className="py-2 px-8 rounded-full text-white bg-cyan-500 font-bold hover:drop-shadow-lg">Generate Flashcards</button>
+              </Link>
             </div>
           </div>
         </section>
 
         <section id="how-it-works" className="w-screen flex flex-col items-center bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)]">
-          <h2 className="text-3xl font-bold">How it works?</h2>
-          <video height={480} width={640} controls>
+          <h2 className="text-4xl font-bold tracking-tighter mb-8">How it works?</h2>
+          <video controls className="rounded-md">
             <source src="/samplevid.mp4" type="video/mp4" />
           </video>
         </section>
 
         <section id="feature" className="w-screen flex flex-col items-center bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)] pt-12">
-          <h2 className="text-3xl font-bold ">Why Choose FlashQuizzr?</h2>
+          <h2 className="text-4xl font-bold tracking-tighter">Why Choose FlashQuizzr?</h2>
 
           <FeatureComponent title="AI-Driven Question Generation" paragraph="Save time and enhance your study sessions with AI-generated quizzes and flashcards tailored to your curriculum. No more manual creation—just instant, high-quality study materials." img_src="/samplepic.jpg" />
 
@@ -56,9 +56,9 @@ export default function Home() {
         </section>
 
         <section id="testemonial" className="w-screen flex flex-col items-center bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)]">
-          <h2 className="text-3xl font-bold">What Our Users Are Saying</h2>
+          <h2 className="text-4xl font-bold tracking-tighter">What Our Users Are Saying</h2>
           <p className="mt-8 text-base text-slate-700">Hear the feedback from students who have achieved their academic goals with us</p>
-          <div className="flex w-2/3 gap-x-12 mt-12">
+          <div className="flex flex-wrap w-2/3 gap-x-12 mt-12">
             <TestimonialComponent reviewerName="Sara L" reviewer="Student" review_text="This app has transformed my study sessions. The AI-generated quizzes are spot on, and the spaced repetition feature has really helped me retain more information. I highly recommend it!" imgAlt="Sara L review" imgSrc="/profile_img.svg" />
             <TestimonialComponent reviewerName="Mark R" reviewer="High School Student" review_text="As a busy student, I don't have time to create study materials from scratch. This app does all the heavy lifting for me, and my grades have never been better. It's a game-changer." imgAlt="Mark R pic" imgSrc="/profile_img.svg" />
             <TestimonialComponent reviewerName="Emily K" reviewer="Graduate Student" review_text="The seamless integration with Anki makes it so easy to stay on top of my study goals. The AI-generated questions are incredibly accurate and relevant to my courses." imgSrc="/profile_img.svg" imgAlt="Emily K pic" />
@@ -66,7 +66,7 @@ export default function Home() {
         </section>
 
         <section id="pricing" className="w-screen flex flex-col items-center bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)]">
-          <h2 className="text-3xl font-bold mt-20">Get Started with the Perfect Plan</h2>
+          <h2 className="text-4xl font-bold mt-20 tracking-tighter">Get Started with the Perfect Plan</h2>
           <div className="flex mt-12 gap-x-12">
             <PricingComponent plan_name="Basic" price="5" period="month" access_list={["Generate Quiz", "Generate FlashCards", "Export Quiz and Flashcard", "100K tokens"]} />
             <PricingComponent plan_name="Pro" price="15" period="month" access_list={["All Basic Feature", "Spaced Repetition", "Managing Quiz and Flashcards"]} />
@@ -74,7 +74,7 @@ export default function Home() {
         </section>
 
         <section id="cta" className="w-screen flex flex-col items-center bg-[rgb(236,251,255)] bg-[radial-gradient(circle, rgba(236,251,255,1) 0%, rgba(247,255,239,1) 100%)]">
-          <h2 className="text-3xl font-bold mt-20">Start Learning Today</h2>
+          <h2 className="text-4xl font-bold mt-20">Start Learning Today</h2>
           <p className="text-lg mt-4">Generate Flashcards and Quizzes with a click.</p>
           <Link href={""} className="mt-12 px-4 py-2 bg-cyan-500 rounded-full text-white">Start Free Trial</Link>
         </section>
@@ -82,8 +82,28 @@ export default function Home() {
       </main>
 
       <footer>
-        <div>
+        <div className="flex flex-wrap">
+          <div className="flex flex-col">
+            <div>
+              <h2>FlashQuizzr</h2>
+            </div>
+            <div><p>lorem</p></div>
+            <div className="flex flex-wrap gap-4">
+              {
+                [1, 2, 4, 5, 6].map((item) => {
+                  return (
+                    <div key={item} className="w-8 h-8 bg-black"></div>
+                  )
+                })
 
+              }
+            </div>
+          </div>
+          <div className="grid grid-cols-3">
+            <div className="flex flex-col"></div>
+            <div className="flex flex-col"></div>
+            <div className="flex flex-col"></div>
+          </div>
         </div>
       </footer>
     </>
