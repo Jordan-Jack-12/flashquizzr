adf6c137effeec8ccd29ed6fc7937e1388dc53e9
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2024-07-17 18:51:11 +0530

    added space between image and texts

--- src/components/FeatureComponentReverse.tsx
+++ src/components/FeatureComponentReverse.tsx
@@ -8,9 +8,9 @@ type PropsType = {
 
 export function FeatureComponentReverse({ title, paragraph, img_src }: PropsType) {
   return (
-    <div className="flex items-start justify-center w-2/3 mx-auto p-16">
+    <div className="flex items-start justify-center w-2/3 mx-auto p-16 gap-x-12">
       <div className="flex-1">
-        <h3 className="text-2xl font-bold">{title}</h3>
+        <h3 className="text-3xl font-bold">{title}</h3>
         <p className="text-base leading-8 mt-4">{paragraph}</p>
       </div>
       <div className="flex-1"><Image src={img_src} alt="feature-image-1" height={360} width={480} /></div>
