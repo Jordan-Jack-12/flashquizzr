caced5fca2c5de5ea2e756253e41a3d694ee52a1
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2024-07-18 18:49:50 +0530

    changed from flex to grid and text color to gray

--- src/components/TestimonialComponent.tsx
+++ src/components/TestimonialComponent.tsx
@@ -10,18 +10,18 @@ type PropsType = {
 
 export function TestimonialComponent({ reviewerName, reviewer, review_text, imgSrc, imgAlt }: PropsType) {
   return (
-    <div className="flex flex-col items-start justify-center flex-1 w-auto rounded-md drop-shadow-lg border bg-white">
+    <div className="flex flex-col items-start justify-start flex-1 min-w-60 rounded-md drop-shadow-lg border bg-white pb-4">
       <div className="flex items-center p-4">
         <div>
           <Image src={imgSrc} alt={imgAlt} height={64} width={64} />
         </div>
-        <div className="flex flex-col m-6">
+        <div className="flex flex-col">
           <p className="text-base font-bold">{reviewerName}</p>
-          <p className="text-sm text-slate-700">{reviewer}</p>
+          <p className="text-sm text-gray-600">{reviewer}</p>
         </div>
       </div>
       <div className="p-4">
-        <p className="text-base">{review_text}</p>
+        <p className="text-base text-gray-800">{review_text}</p>
       </div>
     </div>
   )
