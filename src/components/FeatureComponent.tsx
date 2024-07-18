84ef96fefc7e60354e3a5e4b60988a4cf95e6596
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2024-07-18 18:50:26 +0530

    changed to grid and text color to gray

--- src/components/FeatureComponent.tsx
+++ src/components/FeatureComponent.tsx
@@ -8,11 +8,11 @@ type PropsType = {
 
 export function FeatureComponent({ title, paragraph, img_src }: PropsType) {
   return (
-    <div className="flex items-start justify-center w-2/3 mx-auto p-16 gap-x-12">
-      <div className="flex-1"><Image src={img_src} alt="feature-image-1" height={360} width={480} /></div>
-      <div className="flex-1">
+    <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-items-center container mx-auto p-16 gap-12">
+      <Image src={img_src} alt="feature-image-1" height={360} width={360} />
+      <div className="">
         <h3 className="text-3xl font-bold">{title}</h3>
-        <p className="text-base leading-8 mt-4">{paragraph}</p>
+        <p className="text-base max-w-[450px] text-gray-700 leading-8 mt-4">{paragraph}</p>
       </div>
     </div>
   )
