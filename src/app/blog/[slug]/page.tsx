b0693506eb42bfbfce84ad77ce7e0a1591c7b959
Jordan-Jack-12 <sureshkumarekka53@gmail.com>
2025-09-05 11:23:50 +0530

    added remark plugin to render all html elements

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,11 +2,17 @@
 import { prisma } from "@/lib/prisma"
 import { formatDateToDDMMYYYY } from "@/utils/DateTimeString"
 import { notFound } from "next/navigation"
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
+import rehypeFormat from 'rehype-format'
+import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
+import remarkDirective from 'remark-directive'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
 
 export async function generateStaticParams() {
     const post_slug = await prisma.post.findMany({
@@ -27,7 +33,7 @@ export async function generateMetadata({
     const { slug } = await params
 
     const post = await prisma.post.findUnique({
-        where: { slug: slug},
+        where: { slug: slug },
         select: {
             title: true,
             description: true
@@ -63,11 +69,17 @@ const PostPage = async ({
     }
 
     const content = await unified()
-    .use(remarkParse) // Convert into markdown AST
-    .use(remarkRehype) // Transform to HTML AST
-    .use(rehypeSanitize) // Sanitize HTML input
-    .use(rehypeStringify) // Convert AST into serialized HTML
-    .process(post.content)
+        .use(remarkParse)
+        .use(remarkDirective)
+        .use(remarkFrontmatter)
+        .use(remarkGfm)
+        .use(remarkMath)
+        .use(remarkRehype, { allowDangerousHtml: true })
+        .use(rehypeRaw)
+        .use(rehypeFormat)
+        .use(rehypeSanitize)
+        .use(rehypeStringify) // Convert AST into serialized HTML
+        .process(post.content)
 
     return (
         <main>
